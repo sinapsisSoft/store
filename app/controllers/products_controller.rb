733cf73865b30a6eef3bb8b9ada89b62a65be98e
Sinapsis Soft <sinapsis.soft.developer@gmail.com>
2018-07-17 06:10:10 -0500

    new chages CRUP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,13 +6,30 @@ class ProductsController < ApplicationController
     @products=Product.new
     end
     def create
-        @product=Product.new(product_params)
-        if @product.save
+        @products=Product.new(product_params)
+        if @products.save
             redirect_to products_path
         else
             render :new
         end
     end
+    def edit
+        @products=Product.find(params[:id])
+    end 
+    def update
+        @products=Product.find(params[:id])
+        if @products.update(product_params)
+            redirect_to products_path
+        else
+            render :edit
+        end
+    end 
+    def delete
+        @products=Product.find(params[:id])
+        @products.destroy
+        redirect_to products_path
+       
+    end 
     private 
     def product_params
         params.require(:product).permit(:name, :description, :price)
